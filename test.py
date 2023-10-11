b318d065352877b656cd7cb8e464ecdd199bad3e
Dev  Wankhede <119920311+PrepStation201@users.noreply.github.com>
2023-10-11 21:57:32 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+# Create a Hello world program here
+
+
+
+
+# Import numpy , create an numpy array and print the array
+
+
+
+
+# Import tensorflow and print the current version , create an 2d tensor and print it.
+
+
+
+# Import pandas and print its version and do something with it
